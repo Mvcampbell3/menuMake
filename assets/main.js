69411bd0d8582872e44918faa913eb9dad3416bd
Michael Campbell <mvcampbell3OtherComp@gmail.comgit configcleargit config --global user.name Michael>
2019-01-04 17:56:36 -0500

    1

--- assets/main.js
+++ assets/main.js
@@ -30,8 +30,10 @@ var orangeChix = new Menu("Orange Chicken", 400, [
 var menuList = [chipotleChix, orangeChix];
 
 function displayMenu() {
+    $(".outputArea").html("");
     for (var i = 0; i < menuList.length; i++) {
-        var newDish = $("<div>").attr("class", "dish col-4");
+        var newDish = $("<div>").attr("class", "dish col-4")
+            .attr("data-name", menuList[i].name);
         var dishName = $("<h3>").text("Name: " + menuList[i].name);
         var dishCal = $("<h3>").text("Calories: "+menuList[i].calories);
         var dishIng = $("<h4>").text("Ingredients: "+menuList[i].ingredients.join(", "));
@@ -46,17 +48,23 @@ function displayMenu() {
 
 $(".lunchBtn").on("click", function(){
     console.log("lunch clicked");
+    whichButton = $(this).attr("id");
+    console.log(whichButton);
     $(".menu").fadeIn();
     displayMenu();
 })
 
 $(".dinnerBtn").on("click", function(){
     console.log("dinner clicked");
+    whichButton = $(this).attr("id");
+    console.log(whichButton);
     $(".menu").fadeIn();
     displayMenu();
 })
 
 $(".menu").on("click", ".dish", function(){
-
+    $("#"+whichButton).text($(this).attr("data-name"));
+    console.log($(this));
+    $(".menu").fadeOut();
 })
 
